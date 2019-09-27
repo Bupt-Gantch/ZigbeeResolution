7f8906194587525bdc08cfd392784a739f212c3a
dreamflown <821981739@qq.com>
2019-09-27 15:09:39 +0800

    增加空调预设按键服务

--- src/main/java/com/bupt/ZigbeeResolution/service/InfraredService.java
+++ src/main/java/com/bupt/ZigbeeResolution/service/InfraredService.java
@@ -1,16 +1,5 @@
 package com.bupt.ZigbeeResolution.service;
 
-<<<<<<< HEAD
-import com.bupt.ZigbeeResolution.data.InfraredKey;
-import com.bupt.ZigbeeResolution.data.InfraredPanel;
-import com.bupt.ZigbeeResolution.data.Learn;
-import com.bupt.ZigbeeResolution.mapper.InfraredMapper;
-import com.google.gson.JsonObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-=======
 import com.bupt.ZigbeeResolution.data.AirConditionKey;
 import com.bupt.ZigbeeResolution.data.Key;
 import com.bupt.ZigbeeResolution.data.Learn;
@@ -18,26 +7,18 @@ import com.bupt.ZigbeeResolution.data.Panel;
 import com.bupt.ZigbeeResolution.mapper.InfraredMapper;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import com.google.gson.JsonParser;
 import javafx.util.Pair;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
->>>>>>> devpeng
 
 @Service
 public class InfraredService {
 
-<<<<<<< HEAD
-    @Autowired
-=======
     @Resource
->>>>>>> devpeng
     InfraredMapper irMapper;
 
     public Integer findKey(String deviceId, Integer key) {
@@ -61,13 +42,10 @@ public class InfraredService {
         return irMapper.select_maxkey(deviceId);
     }
 
-<<<<<<< HEAD
-=======
 //    public void deleteKey(String deviceId, Integer key) {
 //        irMapper.delete_a_key(deviceId, key);
 //    }
 
->>>>>>> devpeng
     public void deleteAllKey(String deviceId) {
         irMapper.delete_all_key(deviceId);
     }
@@ -76,88 +54,6 @@ public class InfraredService {
         irMapper.updateStateByResult(deviceId, key);
     }
 
-<<<<<<< HEAD
-    public List<Learn> getCustomerLearns(String deviceId, Integer customerId) {
-        List<Learn> learns = irMapper.selectCustomerLearn(deviceId, customerId);
-        return learns;
-    }
-
-    public List<String> getKeyNames(String deviceId, Integer customerId, Integer panelId) {
-        List<String> keyNames = irMapper.selectKeyNames(deviceId, customerId, panelId);
-        return keyNames;
-    }
-
-    public List<String> getDeviceLearns(String deviceId, Integer panelId){
-        List<String> deviceLearns = irMapper.selectDevicelearns(deviceId,panelId);
-        return  deviceLearns;
-    }
-
-    public List<Learn> getAllLearns(String deviceId){
-        List<Learn> allLearns = irMapper.selectAllLearns(deviceId);
-        return allLearns;
-    }
-
-
-    /**
-     * @author zhs
-     * @time 2019/8/21
-     */
-
-    public List<InfraredPanel> getPanels(String deviceId){
-
-        List<InfraredPanel> panels = null;
-        if (deviceId.equals("")) {
-            return null;
-        }
-
-        try {
-            panels = irMapper.select_all_panels(deviceId);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return panels;
-    }
-
-    public InfraredPanel getPanel(String deviceId, Integer panelId) {
-        InfraredPanel panel = irMapper.select_panel(deviceId, panelId);
-        return panel;
-    }
-
-    public int addPanel(JsonObject data) {
-
-        String deviceId = data.get("deviceId").getAsString();
-        if (deviceId.equals("")){
-            return -1;
-        }
-        String name = data.get("name").getAsString();
-        Integer type = data.get("type").getAsInt();
-
-        InfraredPanel panel = new InfraredPanel();
-        panel.setDeviceId(deviceId);
-        panel.setName(name);
-        panel.setType(type);
-
-        int insert  = irMapper.insert_panel(panel);
-
-        return insert;
-    }
-
-    public int deletePanels(String deviceId) {
-
-        if (deviceId.equals("")) {
-            return -1;
-        }
-
-        int delete = 0;
-        try {
-            List<InfraredPanel> panels = irMapper.select_all_panels(deviceId);
-            for (InfraredPanel panel : panels) {
-                irMapper.delete_all_keys(panel.getPanelId());
-            }
-            delete = irMapper.delete_all_panels(deviceId);
-        } catch (Exception e) {
-            e.printStackTrace();
-=======
     public List<Learn> getCustomerAllLearns(String deviceId, Integer customerId) {
         List<Learn> learns = irMapper.selectCustomerAllLearns(deviceId, customerId);
         return learns;
@@ -191,6 +87,8 @@ public class InfraredService {
         irMapper.delPanels(deviceId,panelIds);
     }
 
+    // =================================================================================
+    // =================================================================================
     // =================================================================================
 
     public Panel findPanel(Integer id){
@@ -257,120 +155,10 @@ public class InfraredService {
             } catch (Exception e){
                 e.printStackTrace();
             }
->>>>>>> devpeng
         }
         return delete;
     }
 
-<<<<<<< HEAD
-    public int deletePanel(String deviceId, Integer panelId) {
-
-        if (deviceId.equals("") || panelId == null) {
-            return -1;
-        }
-
-        int delete = 0;
-        try {
-            irMapper.delete_all_keys(panelId);
-            delete = irMapper.delete_panel(deviceId, panelId);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return delete;
-    }
-
-    public int updatePanel(JsonObject data) {
-
-        String deviceId = data.get("deviceId").getAsString();
-        Integer panelId = data.get("panelId").getAsInt();
-        String name = data.get("name").getAsString();
-        Integer type = data.get("type").getAsInt();
-
-        if (deviceId.equals("") || panelId == null) {
-            return -1;
-        }
-
-        InfraredPanel cachePanel = irMapper.select_panel(deviceId, panelId);
-        if (cachePanel == null) {
-            return -1;
-        }
-
-        InfraredPanel panel = new InfraredPanel();
-        panel.setDeviceId(deviceId);
-        panel.setPanelId(panelId);
-        panel.setName(name);
-        panel.setType(type);
-
-        int update = irMapper.update_panel(panel);
-        return update;
-    }
-
-    public List<InfraredKey> getKeys(Integer panelId) {
-
-        if (panelId == null) {
-            return null;
-        }
-
-        List<InfraredKey> keys = null;
-        try{
-            keys = irMapper.select_all_keys(panelId);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return keys;
-    }
-
-    public InfraredKey addKey(JsonObject data) {
-
-        Integer panelId = data.get("panelId").getAsInt();
-        Integer number = data.get("number").getAsInt();
-        String name = data.get("name").getAsString();
-
-        InfraredKey key = new InfraredKey();
-        key.setPanelId(panelId);
-        key.setNumber(number);
-        key.setName(name);
-
-        int insert = irMapper.insert_key(key);
-        if (insert > 0) {
-            return key;
-        }
-        return null;
-    }
-
-    public int deleteKeys(Integer panelId) {
-
-        if (panelId == null) {
-            return -1;
-        }
-        int delete = irMapper.delete_all_keys(panelId);
-        return delete;
-    }
-
-    public int deleteKey(Integer panelId, Integer keyId) {
-
-        if (panelId == null) {
-            return -1;
-        }
-        int delete = irMapper.delete_key(panelId, keyId);
-        return delete;
-    }
-
-    public int updateKey(JsonObject data) {
-        Integer panelId = data.get("panelId").getAsInt();
-        Integer keyId = data.get("keyId").getAsInt();
-        Integer number = data.get("number").getAsInt();
-        String name = data.get("name").getAsString();
-
-        InfraredKey key = new InfraredKey();
-        key.setPanelId(panelId);
-        key.setKeyId(keyId);
-        key.setNumber(number);
-        key.setName(name);
-
-        int update = irMapper.update_key(key);
-        return update;
-=======
     public int addPanel(String deviceId, JsonObject data) {
         if("".equals(deviceId)) {
             return 0;
@@ -473,14 +261,10 @@ public class InfraredService {
     }
 
     public int deleteKey(String deviceId, Integer key){
-        Pair<Integer, Integer> relation = null;
-        relation = irMapper.select_key_by_deviceId_key(deviceId, key);
+        Pair<Integer, Integer> relation = irMapper.select_key_by_deviceId_key(deviceId, key);
         if (relation == null) {
             return 0;
         }
-//        if (null == findPanel(relation.getKey()) || null == findAKey(relation.getValue())){
-//            return 0;
-//        }
         return deleteKey(relation.getKey(),relation.getValue());
 
     }
@@ -530,7 +314,6 @@ public class InfraredService {
 
     public int updatePanelCondition(Integer panelId, Integer condition){
         try{
-//            new JsonParser().parse(condition);
             return irMapper.update_panel_condition(panelId, condition);
         } catch (JsonParseException e){
             System.err.println("condition is not json string");
@@ -541,7 +324,6 @@ public class InfraredService {
 
     public AirConditionKey getAirConditionKeyAttributes(Integer id){
         return irMapper.select_airconditionKey_attributes(id);
->>>>>>> devpeng
     }
 
 }
