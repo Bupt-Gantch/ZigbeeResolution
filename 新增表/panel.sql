a945919c87aa32f7f9f0f4340801391729ef67a9
dreamflown <821981739@qq.com>
2019-09-27 15:19:09 +0800

    删除无效的表结构

--- 新增表/panel.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `panel` VALUES (1, '空调', 1);
-INSERT INTO `panel` VALUES (2, '电视机', 2);
-INSERT INTO `panel` VALUES (3, '机顶盒', 3);
-INSERT INTO `panel` VALUES (4, 'DVD', 4);
-INSERT INTO `panel` VALUES (5, '冰箱', 5);
