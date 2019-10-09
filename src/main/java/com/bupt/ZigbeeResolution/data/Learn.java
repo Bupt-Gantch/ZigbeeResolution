07e5c1286c4cb3b879509b5f82baaa4629737e3a
dreamflown <821981739@qq.com>
2019-10-09 15:30:27 +0800

    修改属性（暂不使用）

--- src/main/java/com/bupt/ZigbeeResolution/data/Learn.java
+++ src/main/java/com/bupt/ZigbeeResolution/data/Learn.java
@@ -1,25 +1,25 @@
-//package com.bupt.ZigbeeResolution.data;
-//
-//import lombok.Data;
-//
-///**
-// * desciption:遥控面板：功能名
-// */
-//
-//@Data
-//public class Learn {
-//    String name;
-//    Integer key;
-//    Integer panelId;
-//    Integer buttonId;
-//
-//    @Override
-//    public String toString() {
-//        return "Learn{" +
-//                "name='" + name + '\'' +
-//                ", key=" + key +
-//                ", panelId=" + panelId +
-//                ", buttonId=" + buttonId +
-//                '}';
-//    }
-//}
+package com.bupt.ZigbeeResolution.data;
+
+import lombok.Data;
+
+/**
+ * desciption:遥控面板：功能名
+ */
+
+@Data
+public class Learn {
+    String name;
+    Integer key;
+    Integer panelId;
+    Integer buttonId;
+
+    @Override
+    public String toString() {
+        return "Learn{" +
+                "name='" + name + '\'' +
+                ", key=" + key +
+                ", panelId=" + panelId +
+                ", buttonId=" + buttonId +
+                '}';
+    }
+}
